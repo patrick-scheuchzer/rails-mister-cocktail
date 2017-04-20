c4d1f497e41bc55050df4f32515b0c92c4ee2c42
Patrick Scheuchzer <scheuchzer.patrick@gmail.com>
2017-04-20 22:00:24 +0200

    finished seed that imports all ingredients and 10 drinks from 'thecocktaildb.com' with doses'

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 require 'open-uri'
 require 'json'
 
@@ -13,46 +5,50 @@ Cocktail.destroy_all
 Ingredient.destroy_all
 Dose.destroy_all
 
-puts "getting ingredients"
+# Ingredients Import from 'thecocktaildb'
+puts "getting ingredients..."
+
 url_ingredients = 'http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
 ingredients = open(url_ingredients).read
-
 ingredients_hash = JSON.parse(ingredients)
 
-puts "adding ingredients"
+puts "adding ingredients..."
+
 ingredients_hash['drinks'].each do |ingredient|
   Ingredient.create(name: ingredient["strIngredient1"])
 end
 
-puts "finished adding ingredients"
+puts "Added ingredients!"
 
+# Import of 10 random drinks from 'thecocktaildb'
+puts "getting 10 drinks..."
 
-puts "creating 10 drinks"
+drinks = []
 
-
+10.times do
   url_drink = 'http://www.thecocktaildb.com/api/json/v1/1/random.php'
   drink = open(url_drink).read
-
   drink_hash = JSON.parse(drink)
 
+  # sample drink_name --> "Waikiki Beachcomber"
   drink_name = drink_hash['drinks'].first["strDrink"]
+  # sample drink_ingredients --> ["Triple sec", "Gin", "Pineapple juice"]
   drink_ingredients = drink_hash['drinks'].first.select { |k,v| k.match(/strIngredient/) && v != "" }.values
-  drink_measures = drink_hash['drinks'].first.select { |k,v| k.match(/strMeasure/) && v != " " && v != "" }.values
+  # sample drink_measures --> ["3/4 oz", "3/4 oz", "1 tblsp"]
+  drink_measures = drink_hash['drinks'].first.select { |k,v| k.match(/strMeasure/)}.first(drink_ingredients.size).map { |measure| measure.last.strip }
 
-  cocktail = Cocktail.new(name: drink_name)
+  drinks << { name: drink_name, doses: drink_ingredients.zip(drink_measures).map { |dose| { ingredient: dose.first, description: dose.last }} }
+end
 
-  ingredients = drink_ingredients.map { |name| Ingredient.find_by(name: name) }
-  cocktail.ingredients = ingredients
-  cocktail.save
+puts "creating drinks and doses..."
 
+drinks.each do |drink|
+  cocktail = Cocktail.create(name: drink[:name])
+  drink[:doses].each do |dose|
+    Ingredient.create(name: dose[:ingredient]) unless Ingredient.find_by(name: dose[:ingredient])
+    ingredient = Ingredient.find_by(name: dose[:ingredient])
+    Dose.create(description: dose[:description], ingredient: ingredient, cocktail: cocktail)
+  end
+end
 
-
-  # Dose.where(cocktail_id: Cocktail.find_by(name: cocktail.name).id).each_with_index do |dose, index|
-  #     p dose
-  #     p drink_measures[index].strip
-  #   end
-
-
-# drink_name = "Waikiki Beachcomber"
-# drink_ingredients = ["Triple sec", "Gin", "Pineapple juice"]
-# drink_measures = ["3/4 oz ", "3/4 oz ", "1 tblsp "]
+puts "Added drinks and doses!"
